e0603635b510bcd223596231746b2cbe5ae7e290
Emre Cenk <emrecenk9@gmail.com>
2021-06-21 23:27:11 -0400

    all watermarks are removed

--- remover.py
+++ remover.py
@@ -9,11 +9,11 @@ def remove_watermark(path_to_image):
     for i in range(image.size[0]):
         for j in range(image.size[1]):
 
-            if pixelmap[i,j][0:3] == (0,0,0) and pixelmap[i,j][-1] < 100:
+            if pixelmap[i,j][0:3] != (0,0,0) or pixelmap[i,j][-1]<39:
                 pixelmap[i,j] = (255,255,255,255)
 
     print(things)
     image.show()
 
 
-remove_watermark(r"some path")
+remove_watermark(r"C:\Users\Murat\Downloads\tech_1.png")
