574becf2663eeba9a5a8830163e486833efdfa73
Emre Cenk <emrecenk9@gmail.com>
2021-06-21 23:44:58 -0400

    implemented saving and created example image folders

--- remover.py
+++ remover.py
@@ -2,18 +2,21 @@
 from PIL import Image
 
 
-def remove_watermark(path_to_image):
+def remove_watermark(path_to_image, folder_to_save = None, name_to_save = "removed",):
     image = Image.open(path_to_image)
     pixelmap = image.load()
-    things = set()
     for i in range(image.size[0]):
         for j in range(image.size[1]):
 
             if pixelmap[i,j][0:3] != (0,0,0) or pixelmap[i,j][-1]<39:
                 pixelmap[i,j] = (255,255,255,255)
 
-    print(things)
-    image.show()
+
+    if folder_to_save is not None:
+        image.save(folder_to_save + r"\ "[:-1] + name_to_save + ".png")
+    return image
 
 
-remove_watermark(r"C:\Users\Murat\Downloads\tech_1.png")
+remove_watermark(r"imagepath",
+                 r"savepath",
+                 "removed1")
